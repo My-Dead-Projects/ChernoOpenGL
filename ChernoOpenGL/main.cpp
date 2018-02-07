3311e06894c54c933bc5cc22b4ba68f3b2238ef9
Michael Dorst <mikefdorst@mikefdorst.com>
2018-02-06 21:43:23 -0800

    Guard platform specific code

--- ChernoOpenGL/main.cpp
+++ ChernoOpenGL/main.cpp
@@ -13,11 +13,13 @@ int main() {
         cerr << "ERROR: Could not start GLFW3" << endl;
         return 1;
     }
-    
+
+#ifdef __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
     
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
     if (!window) {
