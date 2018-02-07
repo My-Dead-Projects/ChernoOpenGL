ad163aeb471a764b647ef19a798d44ce5e3dd7f7
Michael Dorst <mikefdorst@mikefdorst.com>
2018-02-06 21:18:45 -0800

    Personalize the code

--- ChernoOpenGL/main.cpp
+++ ChernoOpenGL/main.cpp
@@ -1,11 +1,16 @@
-#include <GL/glew.h> // include GLEW and new version of GL on Windows
-#include <GLFW/glfw3.h> // GLFW helper library
-#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+#define uint uint32_t
 
 int main() {
-    // start GL context and O/S window using the GLFW helper library
+    
     if (!glfwInit()) {
-        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        cerr << "ERROR: Could not start GLFW3" << endl;
         return 1;
     }
     
@@ -16,86 +21,80 @@ int main() {
     
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
     if (!window) {
-        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        cerr << "ERROR: could not open window with GLFW3" << endl;
         glfwTerminate();
         return 1;
     }
     glfwMakeContextCurrent(window);
     
-    // start GLEW extension handler
     glewExperimental = GL_TRUE;
     glewInit();
     
     // get version info
-    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-    const GLubyte* version = glGetString(GL_VERSION); // version as a string
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported %s\n", version);
+    auto renderer = glGetString(GL_RENDERER);
+    auto version = glGetString(GL_VERSION);
+    cout << endl << endl
+    << "Renderer: " << renderer << endl
+    << "OpenGL version supported: " << version << endl;
     
-    // tell GL to only draw onto a pixel if the shape is closer to the viewer
-    glEnable(GL_DEPTH_TEST); // enable depth-testing
-    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-    
-    /* OTHER STUFF GOES HERE NEXT */
+    glEnable(GL_DEPTH_TEST);
+    // depth-testing interprets a smaller value as "closer"
+    glDepthFunc(GL_LESS);
     
     float points[] = {
-        0.0f,  0.5f,  0.0f,
-        0.5f, -0.5f,  0.0f,
-        -0.5f, -0.5f,  0.0f
+         0.0,  0.5,  0.0,
+         0.5, -0.5,  0.0,
+        -0.5, -0.5,  0.0
     };
     
-    GLuint vbo = 0;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    uint vertexBufferObject = 0;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
     glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
     
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    uint vertexArrayObject = 0;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     
-    const char* vertex_shader =
+    auto vertex_shader =
     "#version 400\n"
     "in vec3 vp;"
     "void main() {"
     "  gl_Position = vec4(vp, 1.0);"
     "}";
     
-    const char* fragment_shader =
+    auto fragment_shader =
     "#version 400\n"
     "out vec4 frag_colour;"
     "void main() {"
     "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
     "}";
     
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    uint vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vertex_shader, NULL);
     glCompileShader(vs);
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    uint fs = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fs, 1, &fragment_shader, NULL);
     glCompileShader(fs);
     
-    GLuint shader_programme = glCreateProgram();
+    uint shader_programme = glCreateProgram();
     glAttachShader(shader_programme, fs);
     glAttachShader(shader_programme, vs);
     glLinkProgram(shader_programme);
     
+    glUseProgram(shader_programme);
+    glBindVertexArray(vertexArrayObject);
+    
     while(!glfwWindowShouldClose(window)) {
-        // wipe the drawing surface clear
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(shader_programme);
-        glBindVertexArray(vao);
-        // draw points 0-3 from the currently bound VAO with current in-use shader
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        // update other events like input handling
         glfwPollEvents();
-        // put the stuff we've been drawing onto the display
         glfwSwapBuffers(window);
     }
     
-    // close GL context and any other GLFW resources
     glfwTerminate();
     return 0;
 }
